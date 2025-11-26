1f632ab3ebe058a2602b86b978205b047077a195
jcezarjs <julio.filho1@hotmail.com>
2025-11-25 22:02:13 -0300

    bomba

--- controllers/product_controller.ts
+++ controllers/product_controller.ts
@@ -3,7 +3,7 @@ import Product from '../models/product_schema';
 import {z} from 'zod';
 
 const productSchema = z.object({
-  name: z.string({ required_error: "O nome é obrigatório"}).min(3, 'O nome deve conter pelo menos 3 caracteres'),
+  name: z.string({ error : "O nome é obrigatório"}).min(3, 'O nome deve conter pelo menos 3 caracteres'),
   description: z.string().min(1, 'A descrição é obrigatória'),
   price: z.number().nonnegative('O preço deve ser um número não negativo'),
   photos: z.array(z.string().url()).optional(),
@@ -81,11 +81,14 @@ export const updateProduct = async (req: Request, res: Response) => {
     res.status(400).json({ message: err.message });
   }
 
+}
+
+
   //Delete a product
 
 export const deleteProduct = async (req: Request, res: Response) => {
     try {
-      const product = await getProductById;(req, res);
+      const product = await Product.findByIdAndDelete(req.params.id);
       if (product == null){
         return res.status(404).json({message: 'Produto não encontrado'});
       }  else {
@@ -96,4 +99,3 @@ export const deleteProduct = async (req: Request, res: Response) => {
       res.status(500).json({ message: err.message });
     }
   }
-}
